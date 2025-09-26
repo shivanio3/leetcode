00a04acdfe87c211e3616cf95afa7df35d162862
shivanio3 <168416132+shivanio3@users.noreply.github.com>
2025-09-27 00:32:52 +0530

    Time: 131 ms (24.66%) | Memory: 26.9 MB (77.99%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -3,8 +3,8 @@ class Solution:
         n=len(prices)
         mp=0
         bb=prices[0]
-        for i in range(1,n):
-            mp=max(mp,prices[i]-bb)
-            bb=min(bb,prices[i])
+        for i in prices:
+            mp=max(mp,i-bb)
+            bb=min(bb,i)
         return mp
         
